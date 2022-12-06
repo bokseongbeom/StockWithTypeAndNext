08f8745f785351e766af5484d06a1805a1f54ffe
bok <vvs9439@naver.com>
2022-12-06 17:36:28 +0900

    ??

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect } from 'react';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+
+const Axios = require('axios');
+
+const apiKey = process.env.REACT_APP_APIKEY
+const apiSecret = process.env.REACT_APP_SECRETEKEY
+
+Axios.get('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT')
+  .then(response => {
+    console.log(response.data.price);
+  })
+
+
   return (
     <div className={styles.container}>
       <Head>
